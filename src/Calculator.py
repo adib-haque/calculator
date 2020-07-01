31bf167237491747f98d92291f6fff83cf86962e
adibhaque <ash33@njit.edu>
2020-07-01 16:48:17 -0400

    Features: Subtraction Test Added

--- src/Calculator.py
+++ src/Calculator.py
@@ -10,14 +10,12 @@ class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x;
         pass
 
     def add(self, a, b):
-        self.result = a + b
-        return addition(a, b)
+        self.result = addition(a, b)
+        return self.result
 
     def subtract(self, a, b):
-        self.result = a - b
-        return subtraction(a, b)
+        self.result = subtraction(a, b)
+        return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -10,16 +10,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_results_property_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(calculator.result, 0)
 
     def test_add_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.add(2, 2), 4)
         self.assertEqual(calculator.result, 4)
 
-    def test_subtract(self):
+    def test_subtract_method_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.subtract(1, 1), 0)
+        self.assertEqual(calculator.subtract(2, 2), 0)
+        self.assertEqual(calculator.result, 0)
 
 
 if __name__ == '__main__':
