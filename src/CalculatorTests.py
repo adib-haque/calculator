14c20e084ad1b5d97e5ab3967fae152a55f3aadf
adibhaque <ash33@njit.edu>
2020-07-01 15:54:37 -0400

    Feature: Setup Calc Tests

---
+++ src/CalculatorTests.py
@@ -0,0 +1,26 @@
+import unittest
+from Calculator import Calculator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+
+    def test_addition(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(1, 1), 2)
+
+    def test_subtraction(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(1, 1), 0)
+
+    def test_results_property(self):
+        calculator = Calculator()
+        calculator.add(2, 1)
+        self.assertEqual(calculator.result, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
