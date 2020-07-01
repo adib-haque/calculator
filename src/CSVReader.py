f057e6f0282b66a90e9c78bb174b845965cb153c
adibhaque <ash33@njit.edu>
2020-07-01 19:22:33 -0400

    Fix: Code Error Fix

--- src/CSVReader.py
+++ src/CSVReader.py
@@ -10,7 +10,7 @@ class CsvReader:
     data = []
 
     def __init__(self, filepath):
-        with open(filepath)) as text_data:
+        with open(filepath) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
         for row in csv_data:
             self.data.append(row)
