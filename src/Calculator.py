320cc2bbe3c395dfa93424472c32cd201f65176c
adibhaque <ash33@njit.edu>
2020-07-01 18:33:31 -0400

    Task: Added Square, Square Root, Division, and Multiplication Functions

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,33 @@
+import math
+
+
 def addition(a, b):
-    return a + b
+    return float(a) + float(b)
 
 
 def subtraction(a, b):
-    return a - b
+    return float(a) - float(b)
+
+
+def multiplication(a, b):
+    return float(a) * float(b)
+
+
+def division(a, b):
+    if int(b) is not 0:
+        return float(a) / float(b)
+    else:
+        return 'Error, Divisor b cannot be 0'
+
+
+def square(a):
+    a = int(a)
+    return math.pow(a, 2)
+
+
+def srt(a):
+    a = float(a)
+    return math.sqrt(a)
 
 
 class Calculator:
@@ -18,4 +42,20 @@ class Calculator:
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def sqr(self, a):
+        self.result = square(a)
+        return self.result
+
+    def sqrt(self, a, b):
+        self.result = srt(a)
         return self.result
