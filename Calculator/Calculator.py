0a8e8b41bbd79390252cd0862ee552de84f19cf6
adibhaque <ash33@njit.edu>
2020-07-07 21:59:50 -0400

    Fix: Csv reader code

---
+++ Calculator/Calculator.py
@@ -0,0 +1,33 @@
+import math
+
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def sqr(self, a):
+        self.result = square(a)
+        return self.result
+
+    def sqrt(self, a):
+        self.result = srt(a)
+        return self.result
--- CsvReader/CSVReader.py
+++ CsvReader/CSVReader.py
@@ -11,16 +11,14 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-        try:
-            with open(abspath(filepath)) as text_data:
-                csv_data = csv.DictReader(text_data, delimiter=',')
-                for row in csv_data:
-                    self.data.append(row)
-        except OSError:
-            print('Cannot open ', filepath)
+
+        with open(abspath(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
         pass
 
-    def return_data_as_class(self, class_name):
+    def return_data_as_objects(self, class_name):
         objects = []
         for row in self.data:
             objects.append(class_factory(class_name, row))
