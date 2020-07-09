418d0c52a671ab38f6c5c844a0ed1ddddb4d3740
adibhaque <ash33@njit.edu>
2020-07-08 21:19:28 -0400

    Feature: Proportion.py formula added

--- Statistics/Proportion.py
+++ Statistics/Proportion.py
@@ -1,8 +1,13 @@
-def proportion(data):
-    len_new = len(data)
-    prop_data = []
-    for x in data:
-        if x > 64:
-            prop_data.append(x)
-    len_prop = len(prop_data)
-    return len_prop / len_new
+def proportion(propList):
+    try:
+        result = 0
+        num = 0
+        for i in propList:
+            if (i % 2) == 0:
+                num = num + 1
+
+            result = num / len(propList)
+        return result
+    except Exception as e:
+        print("Error in Proportion", e)
+        return 0
