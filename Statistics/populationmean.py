d8b6a96f6d565af8814f7dbf1f4d8975ab5f8afb
adibhaque <ash33@njit.edu>
2020-07-08 21:11:27 -0400

    Fix: pop mean formula error

--- Statistics/populationmean.py
+++ Statistics/populationmean.py
@@ -1,7 +1,11 @@
 from Calculator.Division import division
+from Calculator.Addition import addition
 
 
 def population_mean(data):
-    n = len(data)
-    sum_total = sum(data)
-    return round(division(n, sum_total), 8)
+    numbers = len(data)
+    total = 0
+    for numbers in data:
+        total = addition(total, numbers)
+    return round(division(numbers, total), 8)
+
