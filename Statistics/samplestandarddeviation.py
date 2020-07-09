e7e39d8083df69ebaecfdd6826111b7ceb987325
adibhaque <ash33@njit.edu>
2020-07-08 21:38:39 -0400

    Feature: Standard Deviation

--- Statistics/samplestandarddeviation.py
+++ Statistics/samplestandarddeviation.py
@@ -1,8 +1,23 @@
-def samplestandarddeviation(num):
-    try:
-        variance_float = variance(num)
-        return round(square_root(variance_float), 5)
-    except ZeroDivisionError:
-        print("Error: Can't Divide by 0")
-    except ValueError:
-        print("Error: Check your data inputs")
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+from Calculator.SquareRoot import sq_rt
+from Calculator.Square import square
+from Statistics.sampledata import get_sample
+from Statistics.populationmean import population_mean
+import random
+
+
+def sample_std_dev(data):
+    total = 0
+    samples = random.randint(1, len(data))
+    new_samples = get_sample(data, samples)
+    new_mean = population_mean(new_samples)
+    for number in new_samples:
+        result = subtraction(number, new_mean)
+        sq = square(result)
+        total = addition(total, sq)
+    n = len(new_samples)
+    d = division(subtraction(1, n), total)
+    sample_sd = sq_rt(d)
+    return sample_sd
