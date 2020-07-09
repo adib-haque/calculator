cbcec4ae3a857a578d9ccb133a2acd916fbc2ffc
adibhaque <ash33@njit.edu>
2020-07-08 21:41:51 -0400

    Fix: Variance Population Formula Update

--- Statistics/variancepopulationproportion.py
+++ Statistics/variancepopulationproportion.py
@@ -1,3 +1,9 @@
+from Statistics.Proportion import proportion
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+from Calculator.Multiplication import multiplication
+
+
 def var_pop_proportion(data):
     p = proportion(data)
     q = subtraction(1, p)
