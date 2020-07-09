0aaf245686fa3e2473ab054f0bf26311e7a7e54a
adibhaque <ash33@njit.edu>
2020-07-08 22:00:22 -0400

    Fix: samplevariance.py code fix

--- Statistics/samplevariance.py
+++ Statistics/samplevariance.py
@@ -7,12 +7,12 @@ from Calculator.Multiplication import multiplication
 
 def var_sample_proportion(data):
     sample_data = data[0:999]
-    samp_prop_data = []
+    sample_prop_data = []
     for x in sample_data:
         if x > 64:
-            samp_prop_data.append(x)
-    samp_len = len(samp_prop_data)
-    samp_len_data = len(sample_data)
-    p = round(samp_len / samp_len_data, 6)
+            sample_prop_data.append(x)
+    sample_len = len(sample_prop_data)
+    sample_len_data = len(sample_data)
+    p = round(sample_len / sample_len_data, 6)
     q = subtraction(1, p)
-    return round(multiplication(p, q) / (samp_len_data - 1), 6)
+    return round(multiplication(p, q) / (sample_len_data - 1), 6)
