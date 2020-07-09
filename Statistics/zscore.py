487d4ae9241fe3002ecd37d67444976c18766cc0
adibhaque <ash33@njit.edu>
2020-07-08 21:47:25 -0400

    Fix: Z-Score Formula Update

--- Statistics/zscore.py
+++ Statistics/zscore.py
@@ -1,8 +1,17 @@
+from coverage import data
+from Statistics.populationmean import population_mean
+from Statistics.populationstandarddeviation import pop_stand_dev
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+
+
 def z_score(num):
-    z_mean = populationmean(num)
-    sd = stddev(num)
-    zlist = []
-    for x in num:
-        z = round(division(subtraction(x, z_mean), sd), 6)
-        zlist.append(z)
-    return zlist
+    z_mean = population_mean(data)
+    new_data = [float(x) for x in data]
+    nd = new_data[1]
+
+    pop_std = pop_stand_dev(new_data)
+    y = subtraction(nd, z_mean)
+    z = division(pop_std, y)
+    return z
+
